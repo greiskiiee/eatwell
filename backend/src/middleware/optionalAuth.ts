127b850c62905049b837ba272d38854f3f4536a8
chimegerdene <chimgeebatuh@gmail.com>
2026-05-25 20:14:21 +0800

    lint err fix

--- backend/src/middleware/optionalAuth.ts
+++ backend/src/middleware/optionalAuth.ts
@@ -1,4 +1,4 @@
-import type { NextFunction, Request, Response } from "express";
+import type { NextFunction, Response } from "express";
 import { verifyAccessToken } from "../lib/auth";
 import type { AuthenticatedRequest } from "./auth";
 
